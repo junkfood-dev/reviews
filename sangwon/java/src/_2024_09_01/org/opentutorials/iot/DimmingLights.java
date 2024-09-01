790993ca2463cd28773f8026cb53ba62fc140964
Sang Won Lee <freerusya@naver.com>
2024-09-01 21:56:38 +0900

    feat: study api
    공부용 api 추가

---
+++ sangwon/java/src/_2024_09_01/org/opentutorials/iot/DimmingLights.java
@@ -0,0 +1,16 @@
+package _2024_09_01.org.opentutorials.iot;
+
+public class DimmingLights extends Lighting {
+
+	double _bright;
+	public DimmingLights(String _name) {
+		super(_name);
+		this._bright = 100;
+	}
+	
+	public void setBright(double bright) {
+		this._bright = bright;
+		System.out.println(this._id + " -> DimmingLights bright : "+bright);
+	}
+
+}
---
+++ sangwon/java/src/_2024_09_01/org/opentutorials/iot/OnOff.java
@@ -0,0 +1,6 @@
+package _2024_09_01.org.opentutorials.iot;
+
+public interface OnOff {
+	public boolean on();
+	public boolean off();
+}
