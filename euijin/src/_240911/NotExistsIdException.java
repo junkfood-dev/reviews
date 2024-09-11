2e8ed1b9fa75469710e7b4278f02528182ca9150
ejjeong9103 <ejjeong9103@naver.com>
2024-09-12 00:08:09 +0900

    chore : Today`s quiz
    Quiz

---
+++ euijin/src/_240911/NotExistsIdException.java
@@ -0,0 +1,11 @@
+package _240911;
+
+public class NotExistsIdException extends Exception{
+
+    public NotExistsIdException() {
+
+    }
+    public NotExistsIdException(String message) {
+        super(message);
+    }
+}
