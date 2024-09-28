2056f28735112fd3171a65f8dfb43728b662fb6b
Sang Won Lee <freerusya@naver.com>
2024-09-29 00:24:51 +0900

    feat: add Save reservation

--- sangwon/java/src/_2024_09_28/movie_reservation/Booking.java
+++ sangwon/java/src/_2024_09_28/movie_reservation/Booking.java
@@ -1,5 +1,9 @@
 package _2024_09_28.movie_reservation;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Booking {
     private int id;
     private int showingId;
@@ -46,5 +50,24 @@ public class Booking {
         System.out.println("고객이름: " + getCustomerName());
         System.out.println("상영관 : " + getShowingId());
         System.out.println("예약 좌석: " + getSeats());
+    }
+
+    public void saveReservation(Booking booking) {
+        try (Connection connection = Database.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "INSERT INTO bookings (showing_id, customer_name, seats) VALUES (?, ?, ?)")) {
+            preparedStatement.setInt(1, this.showingId);
+            preparedStatement.setString(2, this.customerName);
+            preparedStatement.setInt(3, this.seats);
+
+            int addRow = preparedStatement.executeUpdate();
+            if (addRow > 0) {
+                System.out.println("예매 정보 저장 완료");
+            } else {
+                System.out.println("예매 정보 저장 실패");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
