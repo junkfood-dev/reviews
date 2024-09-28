97b08c903f6d150e2879427d0dcba738f62a85e8
Sang Won Lee <freerusya@naver.com>
2024-09-28 23:51:05 +0900

    feat: add Showing list search

--- sangwon/java/src/_2024_09_28/movie_reservation/Showing.java
+++ sangwon/java/src/_2024_09_28/movie_reservation/Showing.java
@@ -1,13 +1,15 @@
 package _2024_09_28.movie_reservation;
 
-import java.time.LocalDateTime;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Showing {
     private int id;
     private int movieId;
-    private LocalDateTime showtime;
+    private Timestamp showtime;
 
-    public Showing(int id, int movieId, LocalDateTime showtime) {
+    public Showing(int id, int movieId, Timestamp showtime) {
         this.id = id;
         this.movieId = movieId;
         this.showtime = showtime;
@@ -16,26 +18,37 @@ public class Showing {
     public int getId() {
         return id;
     }
-    public void setId(int id) {
-        this.id = id;
-    }
 
-    public void setMovieId(int movieId) {
-        this.movieId = movieId;
-    }
     public int getMovieId() {
         return movieId;
     }
 
-    public void setShowtime(LocalDateTime showtime) {
-        this.showtime = showtime;
-    }
-    public LocalDateTime getShowtime() {
+    public Timestamp getShowtime() {
         return showtime;
     }
 
     public void displayShowingInfo() {
-        System.out.println("상영관: " + getMovieId());
+        System.out.println("상영관 : " + getMovieId());
         System.out.println("상영 시간: " + getShowtime());
+    }
+
+    public static List<Showing> getAllShowingsByMovie(String movieTitle) {
+        List<Showing> showings = new ArrayList<>();
+        try (Connection connection = Database.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "SELECT * FROM showings AS s INNER JOIN movies AS m ON s.movie_id = m.id WHERE movies.title = ?"
+             )) {
+            preparedStatement.setString(1, movieTitle);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                int movieId = resultSet.getInt("movie_id");
+                Timestamp showtime = resultSet.getTimestamp("showtime");
+                showings.add(new Showing(id, movieId, showtime));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return showings;
     }
 }
