51716c05d37c655c299753cbf4596b23c9db054e
Sang Won Lee <freerusya@naver.com>
2024-09-28 22:07:19 +0900

    feat: add Movie list search

--- sangwon/java/src/_2024_09_28/movie_reservation/Movie.java
+++ sangwon/java/src/_2024_09_28/movie_reservation/Movie.java
@@ -1,5 +1,9 @@
 package _2024_09_28.movie_reservation;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Movie {
     int movieId;
     String title;
@@ -45,5 +49,27 @@ public class Movie {
         System.out.println("영화 : " + getTitle());
         System.out.println("장르 : " + getGenre());
         System.out.println("상영 시간 : " + duration + "분");
+    }
+
+    public static List<Movie> getAllMovies() {
+        List<Movie> movies = new ArrayList<>();
+        try (
+                Connection connection = Database.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "SELECT * FROM movies"
+                );
+                ResultSet resultSet = preparedStatement.executeQuery();
+                ) {
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String title = resultSet.getString("title");
+                String genre = resultSet.getString("genre");
+                int duration = resultSet.getInt("duration");
+                movies.add(new Movie(id, title, genre, duration));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return movies;
     }
 }
