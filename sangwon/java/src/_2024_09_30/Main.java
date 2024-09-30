992e6e2dd015c63f5f44d69664e443c9f0264bad
Sang Won Lee <freerusya@naver.com>
2024-09-30 23:44:21 +0900

    feat: 라이브러리 설치하고 이미지랑 음악 나오게 하기

---
+++ sangwon/java/src/_2024_09_30/Main.java
@@ -0,0 +1,11 @@
+package _2024_09_30;
+
+public class Main {
+
+    public static final int SCREEN_WIDTH = 1280;
+    public static final int SCREEN_HEIGHT = 720;
+
+    public static void main(String[] args) {
+        new Player();
+    }
+}
