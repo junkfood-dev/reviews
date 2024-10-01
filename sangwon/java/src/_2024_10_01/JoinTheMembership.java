9e46fe3ce01ea3f1cdcf08937fe63928e8907a0f
Sang Won Lee <freerusya@naver.com>
2024-10-01 16:42:05 +0900

    feat: add initial class templates

---
+++ sangwon/java/src/_2024_10_01/JoinTheMembership.java
@@ -0,0 +1,9 @@
+package _2024_10_01;
+
+import java.util.Scanner;
+
+public class JoinTheMembership {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
---
+++ sangwon/java/src/_2024_10_01/MembershipManagement.java
@@ -0,0 +1,15 @@
+package _2024_10_01;
+
+public class MembershipManagement {
+
+    public void printMenu() {
+        String menu = """
+                1. 회원가입
+                2. 가입조회
+                3. 회원탈퇴
+                4. 종료
+                메뉴를 선택해주세요: ↵
+                """;
+        System.out.println(menu);
+    }
+}
